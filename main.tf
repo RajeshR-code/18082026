00b6a05c600dd2944a6cb9acebaca5cfbaf08139
Your Name <you@example.com>
2026-08-18 16:47:54 +0000

    rg3 added in West India

--- main.tf
+++ main.tf
@@ -7,3 +7,8 @@ resource "azurerm_resource_group" "rg2" {
   name     = "Nilesh-rg2"
   location = "West Europe"
 }
+
+resource "azurerm_resource_group" "rg3" {
+  name     = "Nilesh-rg3"
+  location = "West India"
+}
